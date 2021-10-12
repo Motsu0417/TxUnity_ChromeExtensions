a0d4ea9e1bc3de1641fcf4dcf75d531af9360667
Motsu0417 <pippi9400@gmail.com>
2021-10-12 16:38:30 +0900

    original tester

--- script.js
+++ script.js
@@ -8,37 +8,20 @@ chrome.tabs.getSelected(tab=>{
 });
 
 window.addEventListener('load',()=>{
-    const txtBox = document.querySelector('input');
-    document.querySelector('button.ttl').addEventListener('click',()=>{
-        txtBox.value = Data.Title;
-    });
-    document.querySelector('button.url').addEventListener('click',()=>{
-        txtBox.value = Data.URL;
-    });
-    document.querySelector('button.bmark').addEventListener('click',()=>{
-        txtBox.value = `[${Data.Title}](${Data.URL})`;
-    });
-
-    const cb = new ClipboardJS('button.copy');
-    const msgContainer = document.querySelector('div.msg');
-    const msgSuccess = document.querySelector('p.copied');
-    const msgFailed = document.querySelector('p.failed');
-    cb.on("success", function(e){
-        console.log('Copied Successfully.', e);
-        msgContainer.style.display = "block";
-        msgSuccess.style.display = "block";
-        setTimeout(()=>{
-            msgSuccess.style.display = "none";
-            msgContainer.style.display = "none";
-        },3000);
-    });
-    cb.on("error", function(e) {
-        console.error('Failed to Copy.', e);
-        msgContainer.style.display = "block";
-        msgFailed.style.display = "block";
-        setTimeout(()=>{
-            msgFailed.style.display = "none";
-            msgContainer.style.display = "none";
-        },3000);
-    });
+    const input_Txname = document.querySelector('input.txname');
+    const input_id = document.querySelector('input.pageId');
+    //txtBox.value = Data.URL;
+    var str_index_start;
+    var str_index_end;
+    if(Data.URL.match(/unityBase/)){
+        str_index_start = Data.URL.indexOf('unityBase');
+    }else{
+        str_index_start = Data.URL.indexOf('unityTips');
+    }
+    str_index_end = Data.URL.indexOf('&step_id')
+    // var str_index_start = Data.URL.indexOf('unityBase');
+    let txName = (Data.URL.substr(str_index_start,str_index_end-str_index_start));
+    let id = Data.URL.substr(str_index_end+6);
+    input_Txname.value = txName;
+    input_id.value = id;
 })
